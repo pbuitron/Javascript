43414bb871ddb5e69a1bab1850d63dfd19300358
pbuitron <pbuitron@users.noreply.github.com>
2022-11-09 02:03:04 -0500

    Primer entregable de Javascritp

--- js/main.js
+++ js/main.js
@@ -1,32 +1,39 @@
-//Se declaran lasa variables para el carrito de compras
+//Se declaran las variables para el carrito de compras
 
 let Televisor, Smartphone, Laptop, Mouse, comprar, continuar, decision, producto, totalcompras;
 Televisor = 2500;
 Smartphone = 1800;
 Laptop = 3500;
-Mouse = 35;
+Mouse = 350;
 comprar = true;
 continuar = true;
 decision = true;
 producto = 0;
 totalcompras = 0;
 
-function decisionDeCompra () {
-    if (decision == true) {
-        comprar = decision
-        producto = Number(prompt(`Marque una de las siguientes opciones:\n1. Televisor - Precio ${Televisor} \n2. Smartphone - Precio ${Smartphone} \n3. Laptop - Precio ${Laptop} \n4. Mouse- Precio ${Mouse}`))
-    } else {
-        decision == false
-        comprar = decision
-        alert(`El total de su compra es ${totalcompras} Soles. Muchas Gracias por su compra\nVuelva Pronto!`)
-        
-    }
+// Se declara funcion para iniciar las compras
+function inicioDeCompras() {
+    producto = Number(prompt(`Marque una de las siguientes opciones:\n1. Televisor - Precio ${Televisor} \n2. Smartphone - Precio ${Smartphone} \n3. Laptop - Precio ${Laptop} \n4. Mouse- Precio ${Mouse}`))
+
 }
 
-//Se solicita un numero valido para iniciar con las compras
-producto = Number(prompt(`Marque una de las siguientes opciones:\n1. Televisor - Precio ${Televisor} \n2. Smartphone - Precio ${Smartphone} \n3. Laptop - Precio ${Laptop} \n4. Mouse- Precio ${Mouse}`))
+// Se declara una función para tomar decisión al momento de realizar la primera compra
+function pregunta1raCompra() {
+    comprar = decision
+    producto = Number(prompt(`Marque una de las siguientes opciones:\n1. Televisor - Precio ${Televisor} \n2. Smartphone - Precio ${Smartphone} \n3. Laptop - Precio ${Laptop} \n4. Mouse- Precio ${Mouse}`))
+}
+//Se declara una función para calcular el monto total y finalizar la compra
+function pregunta2daCompra() {
+    comprar = decision
+    alert(`El total de su compra es ${totalcompras} Soles. Muchas Gracias.\nVuelva Pronto!`)
+}
 
-while (comprar === true) {
+
+
+//Se solicita un numero valido para iniciar con las compras
+inicioDeCompras()
+
+while (comprar == true) {
     switch (producto) {
         case 1:
             totalcompras = totalcompras + Televisor
@@ -41,23 +48,17 @@ while (comprar === true) {
             totalcompras = totalcompras + Mouse
             break;
         default:
-            producto = Number(prompt(`Te equivocaste\nMarque una de las siguientes opciones:\n1. Televisor - Precio ${Televisor} \n2. Smartphone - Precio ${Smartphone} \n3. Laptop - Precio ${Laptop} \n4. Mouse- Precio ${Mouse}`))
+            producto = Number(prompt(
+                `Te equivocaste !!! \nMarque una de las siguientes opciones:\n1. Televisor - Precio ${Televisor} \n2. Smartphone - Precio ${Smartphone} \n3. Laptop - Precio ${Laptop} \n4. Mouse- Precio ${Mouse}`))
             continue;
     }
 
-
-   /* if (producto == 1) {
-         totalcompras = totalcompras + Televisor
-     } else if (producto == 2) {
-         totalcompras = totalcompras + Smartphone
-     } else if (producto == 3) {
-         totalcompras = totalcompras + Laptop
-     } else if (producto == 4) {
-         totalcompras = totalcompras + Mouse
-     } else {
-         producto = prompt(`Te equivocaste\nMarque una de las siguientes opciones:\n1. Televisor - Precio ${Televisor} \n2. Smartphone - Precio ${Smartphone} \n3. Laptop - Precio ${Laptop} \n4. Mouse- Precio ${Mouse}`)
-         continue
-     }*/
     decision = confirm(`Desea continuar comprando`)
-  decisionDeCompra()
+    if (decision == true) {
+        pregunta1raCompra()
+    } else {
+        decision == false
+        pregunta2daCompra()
+
+    }
 }
